fe9164c7f016c2cff880601457f56453cf0a63fd
bemar24 <joa.berriel.24@lehre.mosbach.dhbw.de>
2025-07-07 20:34:24 +0200

    Local Network set-up

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -12,7 +12,7 @@ import {
   ShareListRequest
 } from '../types';
 
-const API_BASE_URL = 'http://localhost:8080/api';
+const API_BASE_URL = '/api';
 
 // Create axios instance
 const api = axios.create({
